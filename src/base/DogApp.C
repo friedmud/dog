f15eac0c443c8ddf7f166bafe20cd20ce0d91658
Derek Gaston <derek.gaston@inl.gov>
2014-04-14 12:10:27 -0600

    initial creation of dog

--- src/base/DogApp.C
+++ src/base/DogApp.C
@@ -1,45 +1,45 @@
-#include "StorkApp.h"
+#include "DogApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
 #include "ModulesApp.h"
 
 template<>
-InputParameters validParams<StorkApp>()
+InputParameters validParams<DogApp>()
 {
   InputParameters params = validParams<MooseApp>();
   return params;
 }
 
-StorkApp::StorkApp(const std::string & name, InputParameters parameters) :
+DogApp::DogApp(const std::string & name, InputParameters parameters) :
     MooseApp(name, parameters)
 {
   srand(libMesh::processor_id());
   
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
-  StorkApp::registerObjects(_factory);
+  DogApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
   ModulesApp::associateSyntax(_syntax, _action_factory);
-  StorkApp::associateSyntax(_syntax, _action_factory);
+  DogApp::associateSyntax(_syntax, _action_factory);
 }
 
-StorkApp::~StorkApp()
+DogApp::~DogApp()
 {
 }
 
 void
-StorkApp::registerApps()
+DogApp::registerApps()
 {
-  registerApp(StorkApp);
+  registerApp(DogApp);
 }
 
 void
-StorkApp::registerObjects(Factory & factory)
+DogApp::registerObjects(Factory & factory)
 {
 }
 
 void
-StorkApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+DogApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
 {
 }
